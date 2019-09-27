06af601684f62506fb99d98ea9ac1dcdbfe35d27
Oren Eno <Oren@localhost.localdomain>
2019-09-27 17:00:34 +0200

    Classmetode added.. method using cls as first arg

--- Learn Python 3 The Hard Way/ex42 - Is-a, Has-a, Object and Classes.py
+++ Learn Python 3 The Hard Way/ex42 - Is-a, Has-a, Object and Classes.py
@@ -1,6 +1,7 @@
 class Employee:
 
     # This is a Class variable
+    num_of_emps = 0
     raise_amount = 1.04
 
     # dunder __init__ takes arguments
@@ -11,6 +12,8 @@ class Employee:
         self.pay = pay
         self.email = first + last + '@company.com'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         # The 'self' in 'self.first/last' is used because then this method will work with other instances
         return '{} {}'.format(self.first, self.last)
@@ -19,11 +22,26 @@ class Employee:
         # Class variables (raise_amount) can be access throught the Class itself ('Employee.) or a instance of that clas ('self.')
         self.pay = int(self.pay * self.raise_amount)
 
+    # This decorator makes sure that we use the class as the first argument instead of an instance.
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    # Alternative Constructor
+    @classmethod
+    def from_string(cls, string):
+        first, last, pay = string.split('-')
+        return cls(first, last, pay)
+        # This will create and return the new employee. Like the creation of an instance (Employee(frist, last, pay))
+
 
 # Create instanse of Class Employee
 emp_1 = Employee('Michael', 'Python', 50000)  # Initsiate with init arugments
 emp_2 = Employee('Maud', 'Ruby', 40000)  # Initsiate with init arugments
 
+#==================================================================#
+#==================================================================#
+#==================================================================#
 
 print(emp_1.fullname())
 print(emp_1.email, '\n')
@@ -58,13 +76,39 @@ print(emp_1.__dict__)
 
 emp_1.raise_amount = 1.05    # Creats a new attribute only in this instance!
 print(Employee.raise_amount)
-print(emp_1.raise_amount)    
+print(emp_1.raise_amount)
 print(emp_1.__dict__)        # You'll see that the raise_amount has been added.
-                             # This way the class doesn't need to  be enfoked
+# This way the class doesn't need to  be enfoked
 
 # Change class variable (for the entire class and subclasses)
-Employee.raise_amount = 1.05 # Creates a new attribute for the entire class.
+Employee.raise_amount = 1.05  # Creates a new attribute for the entire class.
 print(Employee.raise_amount)
-print(emp_1.raise_amount)
+print(emp_1.raise_amount, '\n')
 #==================================================================#
 
+print('Number of employees:', Employee.num_of_emps)
+#==================================================================#
+#==================================================================#
+#==================================================================#
+
+# Using the Class Method (@classmetod)
+Employee.set_raise_amount(1.05)
+
+print(Employee.raise_amount)
+print(emp_2.raise_amount, '\n')
+# #==================================================================#
+
+# Using Alternative Constructors
+
+new_emp_1 = 'John-Doe-90000'
+new_emp_2 = 'Kim-Jane-80000'
+new_emp_3 = 'Doe-John-190000'
+
+
+emp_3 = Employee.from_string(new_emp_1)
+emp_4 = Employee.from_string(new_emp_2)
+emp_5 = Employee.from_string(new_emp_3)
+
+print(emp_3.fullname())
+print(emp_4.fullname())
+print(emp_5.fullname())
