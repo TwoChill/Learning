5d8b81c026ea52d897472536f9146d9756111a5c
Oren Eno <Oren@localhost.localdomain>
2019-09-30 12:57:36 +0200

    A quicker way to write the same thing

--- Head First Python/ex2 - Vowels and Nests.py
+++ Head First Python/ex2 - Vowels and Nests.py
@@ -1,9 +1,15 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = input('Give me a word to show you its vowels :> ')
 found = []
-for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found.append(letter)
+
+# for letter in word:
+#     if letter in vowels:
+#         if letter not in found:
+#             found.append(letter)
+
+found = [letter for letter in word if letter in vowels]
+
 for vowels in found:
     print(vowels)
+
+
